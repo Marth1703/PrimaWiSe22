860929a60052e2fbebe68a5c15ad77c35d1ff129
Marth1703 <marvin.fischer@hs-furtwangen.de>
2022-10-17 14:27:29 +0200

    MarioWalking

--- Mario/Script/Source/Main.ts
+++ Mario/Script/Source/Main.ts
@@ -4,17 +4,27 @@ namespace Script {
 
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
-
+  let marioTransform: ƒ.Node;
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
-
+    
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    console.log(viewport);
+    let branch: ƒ.Node = viewport.getBranch();
+    console.log(branch);
+    marioTransform = branch.getChildrenByName("MarioTransform")[0];
+    console.log("Mario:");
+    
   }
 
+  let tempPos: number = 1;
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
+    tempPos += 0.1;
     ƒ.AudioManager.default.update();
+    marioTransform.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.15 * Math.sin(tempPos));
+    
   }
 }
